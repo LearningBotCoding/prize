3c6b8bfb582aa848b257fe3c4cda66ed681520c8
Legend108 <84610067+LearningBotCoding@users.noreply.github.com>
2021-09-21 12:30:40 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,12 @@
-module.exports = (client) => {
-  console.log(
-    `Logged in as ${client.user.tag}`
-  );
+const mongoose = require('mongoose');
+
+module.exports = async(client) => {
+	const dash = require('../api');
+	dash.dashboard(client);
+
+	console.log(
+		`Logged in as ${client.user.tag}`
+	);
+
+	await mongoose.connect(process.env.mongoURL).then(console.log("MongoDB Connected")).catch(console.error);
 };
